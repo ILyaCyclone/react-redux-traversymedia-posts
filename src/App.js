11ef2025efb813b86025ef3318dc5856e0339448
ILya <ilya.cyclone@gmail.com>
2019-05-06 17:16:38 +0300

    initial setup for redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+
+import store from './store';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostForm/>
-      <Posts/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm/>
+        <Posts/>
+      </div>
+    </Provider>
   );
 }
 
